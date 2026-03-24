8171a6ed7e86db72ac4eb8afe22e139a9590befd
Ricardo Pinas <ricardopinas1@gmail.com>
2026-03-24 20:22:25 -0300

    playerInputs

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerPaddle : MonoBehaviour
 {
+
+    public float speed;
+
+    private Vector2 directionInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,12 @@ public class PlayerPaddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.up * directionInput *speed * Time.deltaTime);
     }
+
+    public void OnMove(InputValue value)
+    {
+        directionInput = value.Get<Vector2>();
+    }
+
 }
